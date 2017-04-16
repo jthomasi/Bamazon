1bff539ffc907171bd0ea39f527a18999e0aa4a6
Jesse Thomas <jayayetea@gmail.com>
2017-04-15 20:45:38 -0500

    handled syntax errors

--- supervisor.js
+++ supervisor.js
@@ -10,7 +10,6 @@ var connection = mysql.createConnection({
 	password:'',
 	database:'Bamazon'
 });
-
 connection.connect(function(err){
 	if(err) throw err;
 	console.log("connected as id "+ connection.threadId);
